e6e9599de6be7ae1d97fb0c24f90be829a59ab8c
fcoverdugo <fcoverdugoa@underpost.net>
2022-12-01 13:11:27 -0300

    update

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_work.Controllers;
+
+[ApiController]
+// [Route("[controller]")]
+[Route("api/WeatherForecast")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [Route("~/api/weatherforecast")]
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+### CRUD REST Service using ASP.NET CORE with JWT Token Authentication
+
+This repository contains the CRUD REST service, built with ASP.NET Core and Entity Framework to illustrate creating REST API to performing CRUD actions and, how to create JWT token and secure API.
+
+#### Prerequisites
+
+- Visual Studio
+- [ASP.NET Core](https://dotnet.microsoft.com/en-us/download)
+- [SQL Server](https://www.microsoft.com/en-us/sql-server/sql-server-downloads)
+
+#### Usage
+
+- create new webapi project `dotnet new webapi`
+- Checkout this project to a location in your disk.
+- Open the solution file using the Visual Studio.
+- Restore the NuGet packages by rebuilding the solution `dotnet restore`.
+- Change the connection string in the appsettings.json file that points to your SQL Server
+- Run the project `dotnet run`.
+
+#### Doc
+
+- https://learn.microsoft.com/en-us/nuget/consume-packages/install-use-packages-dotnet-cli
+
+- https://github.com/SyncfusionExamples/inventory-management-rest-service-asp-dotnet-core
